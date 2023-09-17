da007728eaa6c71154606267d05078416cee1e83
Daeksoul <129008230+Daeksoul@users.noreply.github.com>
2023-09-17 01:27:15 +0100

    Put back to original
    Put back to original, test failed.

--- qb-pizzathis/client/menus.lua
+++ qb-pizzathis/client/menus.lua
@@ -132,8 +132,7 @@ exports['qb-target']:AddBoxZone("pizzathiscooker", vector3(814.24, -752.95, 26.3
     --SAVED FOR FUTURE USE WITH FRIED GOODS
     exports['qb-target']:AddBoxZone("pizzathisfryer", vector3(807.66, -761.21, 26.78), 2.5, 1.5, {
 		name="pizzathisfryer",
-		--heading=35,
-        heading=205,
+		heading=35,
 		debugPoly=false,
 		minZ=26.78,
 		maxZ=28.78,
